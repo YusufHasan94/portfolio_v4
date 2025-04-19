d0b17cd8d8ca86ccc258250eefb18738b4b1d940
YusufHasan94 <yhpolok@gmail.com>
2025-04-19 12:54:33 +0600

    update digital solutions sec

--- src/sections/Services/page.tsx
+++ src/sections/Services/page.tsx
@@ -45,14 +45,17 @@ const Services = () => {
             initial="hidden"
             whileInView={"show"}
             viewport={{ once: true }}
-            className="py-10 lg:py-20 scroll-mt-20"
+            className="pb-10 lg:pb-[106px] scroll-mt-20"
             id="services"
         >
-            <h1 className="text-3xl lg:text-4xl font-semibold text-center">Crafting Digital Solutions</h1>
-            <div className="flex flex-col lg:flex-row py-10 lg:py-20 gap-10">
+            <div className="flex gap-4 items-center">
+                <h1 className="text-[32px] lg:text-[32px] font-semibold text-start">Crafting Digital Solutions</h1>
+                <div className="w-[152px] h-[2px] bg-[#C778DD]"></div>
+            </div>
+            <div className="flex flex-col lg:flex-row pt-10 lg:pt-12 gap-10">
                 {
                     solutions.map((service, index) => (
-                        <div key={index} className="lg:w-1/3 bg-gray-100/90 border-b-4 border-[#6f74ff] rounded-lg px-7 py-4 flex flex-col gap-3">
+                        <div key={index} className="lg:w-1/3  border border-[#ABB2BF] rounded-lg px-7 py-4 flex flex-col gap-3">
                             <Image src={service?.image} alt="" className="w-40 rounded-lg" />
                             <span className="text-2xl font-semibold">{service.title}</span>
                             <p className="text-base font-normal">{service.description}</p>
